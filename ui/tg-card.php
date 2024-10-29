1ec9e91b70d8a996d79db13da3d08489894ae10b
Vladdeek <126556608+Vladdeek@users.noreply.github.com>
2024-10-29 22:57:36 +0200

    ADD страница с телеграмом по ссылке в футере

---
+++ ui/tg-card.php
@@ -0,0 +1,7 @@
+<div class="tg-card d-flex flex-column align-items-center">
+    <div class="tg-photo-container mx-auto">
+        <img class="tg-photo" src="img/default-avatar.jpg" alt="EMPTY" />
+    </div>
+    <h1 class="tg-name text-center">Name</h1>
+    <a href="https://t.me" class="tg-btn">Send Message</a>
+</div>
